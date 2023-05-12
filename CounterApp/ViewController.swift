cb9000b844dd1af418b6426deb67e76c9c76b349
Meir <eagudaev@gmail.com>
2023-05-12 20:08:59 +0300

    Решение базовой задачи

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Text: UILabel!
+    @IBOutlet weak var buttonCount: UIButton!
+    var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Text.text = "Значение счётчика: 0"
     }
 
-
+    @IBAction func buttonCount(_ sender: Any) {
+        count += 1
+        Text.text = "Значение счётчика: " + "\(count)"
+    }
 }
 
